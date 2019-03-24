5da931c1feecd6ab2ed961290367cb481880896c
Zardasht Kaya <zardashtkaya@CypherBits-Laptop.local>
2019-03-24 22:44:21 +0300

    first-stage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+	{ path: '', redirectTo: 'login', pathMatch: 'full' },
+	// { path: 'tabs', loadChildren: './pages/tab1/tab1.module#Tab1PageModule' },
+	{ path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+	{ path: 'tab1', loadChildren: './pages/tab1/tab1.module#Tab1PageModule' },
+	{ path: 'tab2', loadChildren: './pages/tab2/tab2.module#Tab2PageModule' },
+	{ path: 'tab3', loadChildren: './pages/tab3/tab3.module#Tab3PageModule' },
+	{ path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-login',
+	templateUrl: './login.page.html',
+	styleUrls: [ './login.page.scss' ]
+})
+export class LoginPage implements OnInit {
+	constructor() {}
+	server: String = 'TEST_SERVER';
+	ngOnInit() {}
+}
